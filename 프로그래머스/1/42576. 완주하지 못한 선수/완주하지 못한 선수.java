0b9f11e7c692dd19a781e22806305ae3f090ad08
YejiHan <105274970+yejiiH@users.noreply.github.com>
2024-01-08 14:36:53 +0900

    Delete 프로그래머스 directory

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        return answer;
-    }
-}
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public int solution(int[][] sizes) {
-        int answer = 0;
-        return answer;
-    }
-}
